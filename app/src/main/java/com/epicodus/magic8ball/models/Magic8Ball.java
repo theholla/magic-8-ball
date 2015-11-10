e356495cda3b9af24a6ae21956b0a2068668c09e
Molly Waggett & Diana Holland <student@epicodus.com>
2015-11-10 11:33:57 -0800

    Add string array resource with responses, which are no longer hard-coded into Magic8Ball class.

--- app/src/main/java/com/epicodus/magic8ball/models/Magic8Ball.java
+++ app/src/main/java/com/epicodus/magic8ball/models/Magic8Ball.java
@@ -6,15 +6,8 @@ public class Magic8Ball {
 
     private String[] mResponses;
 
-    public Magic8Ball() {
-        mResponses = new String[]{"It is certain.", "It is decidedly so.",
-                "Without a doubt.", "Yes, definitely.", "You may rely on it.",
-                "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.",
-                "Signs point to yes.", "Reply hazy try again.", "Ask again later.",
-                "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.",
-                "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.",
-                "Very doubtful."};
-
+    public Magic8Ball(String[] responses) {
+        mResponses = responses;
     }
 
     public String getResponse() {
