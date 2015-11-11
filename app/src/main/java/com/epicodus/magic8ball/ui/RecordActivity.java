8ed402fd64b713de0e5dcc927f2b9dd9b09411e4
Molly Waggett & Diana Holland <student@epicodus.com>
2015-11-10 16:21:47 -0800

    Style buttons and clean up

--- app/src/main/java/com/epicodus/magic8ball/ui/RecordActivity.java
+++ app/src/main/java/com/epicodus/magic8ball/ui/RecordActivity.java
@@ -45,7 +45,7 @@ public class RecordActivity extends AppCompatActivity implements AdapterView.OnI
         mRecordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mOutputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/"+mSelectedResponse+".3gp";
+                mOutputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/"+ mSelectedResponse +".3gp";
 
                 recorder = new MediaRecorder();
                 recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
@@ -123,25 +123,4 @@ public class RecordActivity extends AppCompatActivity implements AdapterView.OnI
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_record, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
